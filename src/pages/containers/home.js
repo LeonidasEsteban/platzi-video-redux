cc1ef5361a5cbe2b680dc165e99a892997c7f527
Leonidas Esteban <leonidasesteban@gmail.com>
2017-12-06 19:54:37 -0500

    clase 25 - actualizando un estado inmutable

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -7,6 +7,7 @@ import Modal from '../../widgets/components/modal';
 import HandleError from '../../error/containers/handle-error';
 import VideoPlayer from '../../player/containers/video-player';
 import { connect } from 'react-redux';
+import { List as list } from 'immutable';
 
 class Home extends Component {
   state = {
@@ -57,10 +58,17 @@ function mapStateToProps(state, props) {
   const categories = state.get('data').get('categories').map((categoryId) => {
     return state.get('data').get('entities').get('categories').get(categoryId)
   })
-
+  let searchResults = list()
+  const search = state.get('data').get('search');
+  if (search) {
+    const mediaList = state.get('data').get('entities').get('media');
+    searchResults = mediaList.filter((item) => (
+      item.get('author').toLowerCase().includes(search.toLowerCase())
+    )).toList();
+  }
   return {
     categories: categories,
-    search: state.get('data').get('search')
+    search: searchResults
   }
 
 }
--- src/reducers/data.js
+++ src/reducers/data.js
@@ -4,7 +4,7 @@ import { fromJS } from 'immutable';
 const initialState = fromJS({
   entities: schema.entities,
   categories: schema.result.categories,
-  search: [],
+  search: '',
 })
 
 
@@ -12,18 +12,7 @@ const initialState = fromJS({
 function data(state = initialState, action) {
   switch (action.type) {
     case 'SEARCH_VIDEO': {
-      // action.payload.query
-      let results = [];
-      if (action.payload.query) {
-        const list = state.data.categories[2].playlist;
-        results = list.filter((item) => {
-          return item.author.includes(action.payload.query)
-        })
-      }
-      return {
-        ...state,
-        search: results
-      }
+      return state.set('search', action.payload.query)
     }
     default:
       return state
